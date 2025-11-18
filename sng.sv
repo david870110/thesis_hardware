ebf25a14062fd8d81e2ed21b1e645cd175808a8f
david870110 <david19980110@gmail.com>
2025-11-18 22:25:36 +0800

    fifo.sv

--- sng.sv
+++ sng.sv
@@ -5,22 +5,26 @@ module SNG #(
     parameter STRIDE    = 17,
     parameter QUANT     = 8
 )(
-    input   [QUANT - 1: 0]         iData,
-    output  [BITSTREAM-1 : 0]      oBitstream
+    input   [QUANT - 1: 0]      w_data,
+    input                       clk,
+    input                       rst_n,
+    input                       w_valid,
+    input                       r_ready,
+    input                       wlast,
+    output  [BITSTREAM-1 : 0]   r_bitstream
 );
-
     logic [$clog2(BITSTREAM) : 0 ] quota_num;
-
-
+    logic [BITSTREAM - 1 : 0] weyl_bit;
+    logic [1:0] k;
+    logic [BITSTREAM - 1 : 0] phase_bit;
 
     QUOTA #(
         .BITSTREAM  (BITSTREAM),
         .QUANT      (QUANT)
     ) quota_dut (
-        .data   (iData),
+        .data   (w_data),
         .quota  (quota_num)
     );
-
 
     WEYL #(
         .BITSTREAM (BITSTREAM),
@@ -28,9 +32,19 @@ module SNG #(
         .STRIDE    (STRIDE)
     ) weyl_dut (
         .quota_num (quota_num),
-        .weyl_out  (oBitstream)
+        .weyl_out  (weyl_bit)
+    );
+    always_ff @(posedge clk or negedge rst_n)
+        if(!rst_n) k <= 0;
+        else if(r_ready & w_valid) k <= k+1;
+                                
+
+    PHASE_2b #(
+        .BITSTREAM  (BITSTREAM)
+    ) dut (
+        .k          (k),
+        .in_bits    (weyl_bit),
+        .out_bits   (phase_bit)
     );
     
-
-
 endmodule
